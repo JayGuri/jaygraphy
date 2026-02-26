c3c5fd66429a5fd92dea4d5056f234ef6f37756e
JayGuri <165677478+JayGuri@users.noreply.github.com>
2026-02-26 21:27:22 +0530

    Minor fixes

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "wsrv.nl" },
-      { protocol: "https", hostname: "**.wsrv.nl" },
-      { protocol: "https", hostname: "**" },
+      { protocol: "https", hostname: "*.wsrv.nl" }, // if you still use wsrv
+      { protocol: "https", hostname: "lvozuswygskgyeixiubu.supabase.co" },
     ],
     formats: ["image/avif", "image/webp"],
   },
